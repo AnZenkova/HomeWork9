29a61ded9e2db2e4fe2581c9346eda291d2536ca
AnZenkova <nastyonka16@mail.ru>
2022-01-26 17:48:01 +1000

    Книги занесены в массив и распечатаны

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,31 +1,54 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
+    public static Book writeBookToArray(Book name, Book arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                arr[i] = name;
+                break;
+            }
+        }
+        return name;
+    }
+
+    public static Book printBooks (Book arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i].getBookTitle());
+            System.out.println(arr[i].getBookAuthor());
+            System.out.println(arr[i].getBookReleaseYear());
+        }
+        return null;
+    }
+
+
     public static void main(String[] args) {
+        Book[] books = new Book[3];
         // 1-я книга
         String bookTitle = "Сияние";
         String nameAuthor = "Стивен";
         String surnameAuthor = "Кинг";
         int bookReleaseYear = 2016;
         Book theShining = new Book(bookTitle, new Author(nameAuthor, surnameAuthor), bookReleaseYear);
-        System.out.println("theShining = " + theShining.getBookAuthor());
-        System.out.println("theShining.getBookTitle() = " + theShining.getBookTitle());
-        System.out.println("theShining.getBookReleaseYear() = " + theShining.getBookReleaseYear());
+        writeBookToArray(theShining,books);
         theShining.setBookReleaseYear(2000);
-        System.out.println("theShining = " + theShining.getBookReleaseYear());
         // 2-я книга
         bookTitle = "Мастер и Маргарита";
         nameAuthor = "Михаил";
         surnameAuthor = "Булгаков";
         bookReleaseYear = 2020;
         Book masterAndMargarita = new Book(bookTitle, new Author(nameAuthor, surnameAuthor), bookReleaseYear);
+        writeBookToArray(masterAndMargarita,books);
         // 3-я книга
         bookTitle = "Гарри Поттер и филосовский камень";
         nameAuthor = "Джоан";
         surnameAuthor = "Роулинг";
         bookReleaseYear = 2000;
         Book harryPotter = new Book(bookTitle, new Author(nameAuthor, surnameAuthor), bookReleaseYear);
-
+        writeBookToArray(harryPotter,books);
+        printBooks(books);
+        Arrays.toString(books);
     }
 }
